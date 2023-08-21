86bd461fc912d15fb957552534d0b5ad9eb359b4
czz <Dchenzizheng@126.com>
2023-08-21 17:59:28 +0800

    修改

--- blog-api/salishBlog-common/src/main/java/com/salishBlog/common/utils/CosUtil.java
+++ blog-api/salishBlog-common/src/main/java/com/salishBlog/common/utils/CosUtil.java
@@ -9,11 +9,12 @@ import com.qcloud.cos.region.Region;
 
 public class CosUtil {
 
-    public static COSClient initCos() {
+
+    public static COSClient initCos(String secretId,String secretKey) {
         // 1 初始化用户身份信息（secretId, secretKey）。
         // SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-        String secretId = SalishConstants.COS_SECRET_ID;//用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
-        String secretKey = SalishConstants.COS_SECRET_KEY;//用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+//        String secretId = SalishConstants.COS_SECRET_ID;//用户的 SecretId，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
+//        String secretKey = SalishConstants.COS_SECRET_KEY;//用户的 SecretKey，建议使用子账号密钥，授权遵循最小权限指引，降低使用风险。子账号密钥获取可参见 https://cloud.tencent.com/document/product/598/37140
         COSCredentials cred = new BasicCOSCredentials(secretId, secretKey);
         // 2 设置 bucket 的地域, COS 地域的简称请参见 https://cloud.tencent.com/document/product/436/6224
         // clientConfig 中包含了设置 region, https(默认 http), 超时, 代理等 set 方法, 使用可参见源码或者常见问题 Java SDK 部分。
